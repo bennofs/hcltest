eaeac21a7e6b41613b7227078fc896fcd57bbbc2
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2014-05-07 18:08:15 +0200

    Fix quoting error

--- travis/github.sh
+++ travis/github.sh
@@ -4,14 +4,14 @@ source travis/util.sh
 
 package=$1
 
-echo "${green}Installing $package from github${nc}"
+echo -e "${green}Installing $package from github${nc}"
 
 tmp=`mktemp -d`  
 git clone "https://github.com/$package" $tmp
 
 olddir=$PWD
 cd $tmp
-cabal-\$CABALVER install -j
+cabal-$CABALVER install -j
 
 cd $olddir
 rm -rf $tmp
