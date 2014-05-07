e3c0174704216816fb4a6fb6d954177c984fa702
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2014-05-07 16:53:37 +0200

    travis-ci: Install exceptions from github

---
+++ travis/github.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+source travis/util.sh
+
+package=$1
+
+step "Installing $package from github" <<'EOF'
+  git clone "https://github.com/$package"
+  cd "$package"
+  cabal-$CABALVER install -j
+  cd ..
+EOF
--- travis/script.sh
+++ travis/script.sh
@@ -1,24 +1,6 @@
 #!/usr/bin/env bash
 
-green='\e[0;32m'
-red='\e[0;31m'
-nc='\e[0m' # No Color
-
-function step {
-  echo -e "${green}$1 ...${nc}"
-  bash /dev/stdin || exit 1
-}
-
-function step_suppress {
-  echo -ne "${green}$1 ... ${nc}"
-  tmp=$(mktemp)
-  bash /dev/stdin &> $tmp && echo -e "${green}Done${nc}" || (
-    echo -e "${red}Failed${nc}"
-    echo "Output: "
-    cat $tmp
-    exit 1
-  )
-}
+source travis/util.sh
 
 step "Configuring project" << 'EOF'
   tmp=$(mktemp)
