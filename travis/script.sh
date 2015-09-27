446ae6b3aab8c6eb9267f1b419d13fba7d2cd2a0
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2015-09-27 21:10:06 +0200

    rewrite travis support

--- travis/script.sh
+++ travis/script.sh
@@ -1,38 +1,56 @@
 #!/usr/bin/env bash
 
-source travis/util.sh
+begin_steps
 
+if [ -n "$ROOT" ]; then
+  step "Checking style with HLint" << EOF
+    hlint --cpp-simple src tests
+EOF
+fi
+
+set -e
 step "Configuring project" << 'EOF'
-  tmp=$(mktemp)
-  cabal-$CABALVER configure --enable-tests --enable-benchmarks -v2 --ghc-options="-Wall -Werror" &> $tmp || (
-    cat $tmp
+  cabal configure --enable-tests --enable-benchmarks -v2 --ghc-options="-Wall -Werror -ddump-minimal-imports" &> cabal.log || (
+    cat cabal.log 
     exit 1
   )
   echo "Using packages: "
-  sed -nre 's/Dependency ([^ ]*) ==([^ :]*).*/\1 \2/p' $tmp | column -t | sed -e "s/^/  /"
+  sed -nre 's/Dependency ([^ ]*) ==([^ :]*).*/\1 \2/p' cabal.log | column -t | sed -e "s/^/  /"
+  echo "Flags chosen: "
+  sed -nr -e ':x; /\,$/ { N; s/,\n/,/; tx }' -e 's/Flags chosen: (.*)/\1/' -e 's/, /,/gp' cabal.log | tr ',' '\n'
 EOF
 
 step "Building project" << EOF
-  cabal-$CABALVER build
+  cabal build
 EOF
 
+set +e
+if [ -n "$ROOT" ]; then
+  step_suppress "Checking for unused dependencies" << EOF
+    packunused
+EOF
+fi
+set -e
+
 step "Running tests" << EOF
-  cabal-$CABALVER test
+  cabal test
 EOF
 
 step "Creating source distribution" << EOF
-  cabal-$CABALVER check
-  cabal-$CABALVER sdist # tests that a source-distribution can be generated
+  cabal check
+  cabal sdist # tests that a source-distribution can be generated
 EOF
 
 step_suppress "Checking source distribution" << 'EOF'
   # The following scriptlet checks that the resulting source distribution can be built & installed
-  SRC_TGZ=$(cabal-$CABALVER info . | awk '{print $2 ".tar.gz";exit}')
+  SRC_TGZ=$(cabal info . | awk '{print $2 ".tar.gz";exit}')
   cd dist/
   if [ -f "$SRC_TGZ" ]; then
-    cabal-$CABALVER install "$SRC_TGZ"
+    cabal install --enable-tests --enable-benchmarks "$SRC_TGZ"
   else
     echo "expected '$SRC_TGZ' not found"
     exit 1
   fi    
 EOF
+
+end_steps
